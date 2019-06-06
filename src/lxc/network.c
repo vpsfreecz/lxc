8f75ae8b932f133f614e3ee2f56687ddc018344b
Jakub Skokan <jakub.skokan@havefun.cz>
2019-06-06 14:52:25 +0200

    [vpsAdminOS] Patch path to lxc-user-nic to use security wrapper

--- src/lxc/network.c
+++ src/lxc/network.c
@@ -2940,7 +2940,7 @@ int lxc_find_gateway_addresses(struct lxc_handler *handler)
 
 #ifdef IN_LIBLXC
 
-#define LXC_USERNIC_PATH LIBEXECDIR "/lxc/lxc-user-nic"
+#define LXC_USERNIC_PATH "/run/wrappers/bin/lxc-user-nic"
 static int lxc_create_network_unpriv_exec(const char *lxcpath,
 					  const char *lxcname,
 					  struct lxc_netdev *netdev, pid_t pid,
