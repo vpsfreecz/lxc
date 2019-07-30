5597d4241ee38729c42f92dde85f441af6133402
Pavel Snajdr <snajpa@snajpa.net>
2019-07-30 14:33:34 +0200

    [WIP] syslog_ns support

--- src/lxc/confile.c
+++ src/lxc/confile.c
@@ -143,6 +143,7 @@ lxc_config_define(tty_max);
 lxc_config_define(tty_dir);
 lxc_config_define(uts_name);
 lxc_config_define(sysctl);
+lxc_config_define(syslogns);
 lxc_config_define(proc);
 
 static int set_config_unsupported_key(const char *key, const char *value,
@@ -252,6 +253,7 @@ static struct lxc_config_t config_jump_table[] = {
 	{ "lxc.tty.max",                    true,  set_config_tty_max,                    get_config_tty_max,                    clr_config_tty_max,                    },
 	{ "lxc.uts.name",                   true,  set_config_uts_name,                   get_config_uts_name,                   clr_config_uts_name,                   },
 	{ "lxc.sysctl",                     false, set_config_sysctl,                     get_config_sysctl,                     clr_config_sysctl,                     },
+	{ "lxc.syslogns",                   true,  set_config_syslogns,                   get_config_syslogns,                   clr_config_syslogns,                   },
 	{ "lxc.proc",                       false, set_config_proc,                       get_config_proc,                       clr_config_proc,                       },
 };
 
@@ -1985,6 +1987,32 @@ static int set_config_sysctl(const char *key, const char *value,
 
 	lxc_list_add_elem(sysctl_list, move_ptr(sysctl_elem));
 	lxc_list_add_tail(&lxc_conf->sysctls, move_ptr(sysctl_list));
+
+	return 0;
+}
+
+static int set_config_syslogns(const char *key, const char *value,
+			       struct lxc_conf *lxc_conf, void *data)
+{
+	int makenew;
+
+	if (lxc_config_value_empty(value)) {
+		makenew = false;
+		return 0;
+	}
+
+	if (value[0] >= '0' && value[0] <= '1') {
+		if (lxc_safe_int(value, &makenew) < 0)
+			return -1;
+	} else {
+		if (lxc_safe_int(value, &makenew) == 1)
+			makenew = true;
+	}
+
+	/* Store these values in the lxc_conf, and then try to set for actual
+	 * current logging.
+	 */
+	lxc_conf->syslogns = (bool)makenew;
 
 	return 0;
 }
@@ -4280,6 +4308,12 @@ static int get_config_sysctl(const char *key, char *retv, int inlen,
 	return fulllen;
 }
 
+static int get_config_syslogns(const char *key, char *retv, int inlen,
+			       struct lxc_conf *c, void *data)
+{
+	return c->syslogns;
+}
+
 static int get_config_proc(const char *key, char *retv, int inlen,
 			   struct lxc_conf *c, void *data)
 {
@@ -4845,6 +4879,13 @@ static inline int clr_config_sysctl(const char *key, struct lxc_conf *c,
 				   void *data)
 {
 	return lxc_clear_sysctls(c, key);
+}
+
+static inline int clr_config_syslogns(const char *key, struct lxc_conf *c,
+				      void *data)
+{
+	c->syslogns = false;
+	return 0;
 }
 
 static inline int clr_config_proc(const char *key, struct lxc_conf *c,
